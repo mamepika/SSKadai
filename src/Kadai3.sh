7e1b1d3000e7d59a025a6d896fd876e7586f7d02
ta-satoh <starlightprayer@gmail.com>
2019-05-28 19:13:44 +0900

    first commit

---
+++ src/Kadai3.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+if [[ $# != 2 ]]
+then
+  echo "引数は2つ指定してください"
+  exit
+fi
+
+gojo()
+{
+  if[[ $1 < $2 ]]
+  then
+    a=$2
+    b=$1
+  else
+    a=$1
+    b=$2
+  fi
+  
+  
+  while [[ $b != 0 ]]
+  do
+    r=$((a%b))
+    a=$b
+    b=$r
+  done
+  echo $a
+}
+
+gojo "$1" "$2"
+
