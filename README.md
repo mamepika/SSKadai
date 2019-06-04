47dc2c6dd7aa80462434a346a64ddb8ead785ab4
ta-satoh <starlightprayer@gmail.com>
2019-06-04 18:58:47 +0900

    fix typo

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 ## Kadai3.sh
 2つの引数の最大公約数を表示するシェルスクリプト
+
+## Test_Kadai3.sh
+Kadai3.shをテストする、テスト用シェルスクリプト
+
+## .travis.yml
+Travis CI用のymlファイル
--- Test_Kadai3.sh
+++ Test_Kadai3.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-result=$(./kadai3.sh 64 16)
+result=$(./Kadai3.sh 64 16)
 if [ $result = 16 ] ; then
 	echo "test1 OK"
 else
@@ -8,7 +8,7 @@ else
 	exit 1
 fi
 
-result=$(./kadai3.sh 32 12)
+result=$(./Kadai3.sh 32 12)
 if [ $result = 4 ] ; then
 	echo "test2 OK"
 	exit 0
